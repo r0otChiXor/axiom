6190117586b9c41f956c8e30a11592033e20c18e
Gavin Hurlbut <gjhurlbu@gmail.com>
2018-03-03 13:28:26 -0800

    Oops, wrong path

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,7 +1,7 @@
 package=expat
 $(package)_version=2.2.1
 $(package)_tag=R_2_2_1
-$(package)_download_path=https://github.com/libexpat/libexpat/releases/tag/$($(package)_tag)
+$(package)_download_path=https://github.com/libexpat/libexpat/releases/download/$($(package)_tag)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=1868cadae4c82a018e361e2b2091de103cd820aaacb0d6cfa49bd2cd83978885
 
