1e41b7777b86f32a573c5d48ec04e313a0bc49ed
Gavin Hurlbut <gjhurlbu@gmail.com>
2018-05-30 22:32:18 -0700

    Rename linux zip file to include HOST

--- contrib/travis-artifacts/collect-artifacts.sh
+++ contrib/travis-artifacts/collect-artifacts.sh
@@ -20,7 +20,7 @@ ZIPFILES=$(ls $OUTDIR/bin/* || true)
 
 # Linux artifacts
 [ -z "$ZIPFILES" ] || \
-       zip -uj $RELEASEDIR/castle-$COMMIT.zip ${ZIPFILES}
+       zip -uj $RELEASEDIR/castle-$COMMIT-$HOST.zip ${ZIPFILES}
 
 # MaxOSX artifacts
 cp -a Castle-Core.dmg $RELEASEDIR || true
