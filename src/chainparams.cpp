d1d11c4601f5dce5e2853139c074d076f0de8b75
Gavin Hurlbut <gjhurlbu@gmail.com>
2018-06-11 21:28:24 -0700

    Bump ZeroCoin v2 to block 21

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -144,7 +144,7 @@ public:
         nBlockEnforceInvalidUTXO = 1; //Start enforcing the invalid UTXO's
 
         nInvalidAmountFiltered = 0 * COIN; //Amount of invalid coins filtered through exchanges, that should be considered valid
-        nBlockZerocoinV2 = 1; //!> The block that zerocoin v2 becomes active - roughly Tuesday, May 8, 2018 4:00:00 AM GMT
+        nBlockZerocoinV2 = 21; //!> The block that zerocoin v2 becomes active - roughly Tuesday, May 8, 2018 4:00:00 AM GMT
         nEnforceNewSporkKey = 1525158000; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM
 
